8fe66796eecad98f749f91041b2dde78ec4ce855
Tijs <68697633+tijsnoordhof@users.noreply.github.com>
2024-12-16 13:33:40 +0100

    first part hero section

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="container mx-44 flex justify-center">
+      <div className="flex flex-col items-center border w-full">
+        <h1 className="text-[32px] font-semibold text-center text-base">Uw Allround Specialist Voor Huis En Dak</h1>
+        <p className="text-[16px] text-center text-accent">Professionele renovatie en onderhoud voor uw woning, van kleine klussen tot complete dakprojecten</p>
+      </div>
     </div>
   );
 }
