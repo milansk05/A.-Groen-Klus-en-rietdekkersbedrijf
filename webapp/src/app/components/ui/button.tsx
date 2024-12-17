fdcfd5ddaddbec96329c50db957db1bf2ee42c65
Milan Snoeijink <snoeijinkmilan@gmail.com>
2024-12-17 13:28:12 +0100

    Toevoeging van button component

---
+++ webapp/src/app/components/ui/button.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+<div className="pt-4">
+    <Link
+        href="/contact"
+        className="inline-block bg-primary text-white px-6 py-3 rounded-md hover:bg-opacity-90 transition-colors"
+    >
+        Button
+    </Link>
+</div>
