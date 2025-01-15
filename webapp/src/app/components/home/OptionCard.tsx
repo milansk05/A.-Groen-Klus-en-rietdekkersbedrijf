d33384676f6539e7d022dde99237c1dc3b6fb03d
Milan Snoeijink <snoeijinkmilan@gmail.com>
2025-01-15 13:13:57 +0100

    Bijgewerkte Homepagina

--- webapp/src/app/components/home/OptionCard.tsx
+++ webapp/src/app/components/home/OptionCard.tsx
@@ -1,25 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 
 interface PupilArray {
-    text: string;
+  text: string;
 }
 
 interface OptionCardProps {
-    title: string;
-    image: string;
+  title: string;
+  image: string;
 }
 
-const OptionCard:React.FC<OptionCardProps> = ({title, image}) => {
+const OptionCard: React.FC<OptionCardProps> = ({ title, image }) => {
   return (
-    <div className={`w-[25rem] h-[12rem] bg-[url('/${image}')] bg-cover rounded-lg px-8 py-6 flex flex-col justify-between`}>
+    <div className="relative w-[25rem] h-[12rem] rounded-lg overflow-hidden">
+      <Image
+        src={`/${image}`}
+        alt={title}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute inset-0 px-8 py-6 flex flex-col justify-between bg-black bg-opacity-40">
         <h2 className="text-white text-[22px] font-semibold">{title}</h2>
         <div className="text-white flex flex-wrap gap-2 text-sm">
-            <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg text-black">kozijnen</span>
-            <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg text-black">zonnepanelen</span>
-            <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg text-black">renovaties</span>
-            <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg text-black">tuinen</span>
-            <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg text-black">daken</span>
+          <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg">kozijnen</span>
+          <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg">zonnepanelen</span>
+          <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg">renovaties</span>
+          <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg">tuinen</span>
+          <span className="bg-white/20 px-3 py-1 border rounded-full border-white/50 backdrop-blur-lg">daken</span>
         </div>
+      </div>
     </div>
   )
 }
